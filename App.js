f868d1688f76f61dc7fc233837ce186a8c76aaf3
ountana <smileygirl17032001@gmail.com>
2024-02-19 10:15:50 +0000

    lr2

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [buttonColor, setButtonColor] = useState("#62a87c");
+  const handleTextPress = () => {
+    Alert.alert("You clicked on the text", "Press ok", [
+      {
+        text: "ok",
+        onPress: () => console.log("closed"),
+      },
+    ]);
+  };
+  const handleButtonPress = () => {
+    const sameColor = "#255957".toString(16);
+    setButtonColor(sameColor);
+    console.log(sameColor);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working</Text>
       <StatusBar style="auto" />
+      <TouchableHighlight
+        activeOpacity={0.6}
+        nderlayColor={"#093A3E"}
+        onPress={handleTextPress}
+      >
+        <Text numberOfLines={1} style={styles.text}>
+          Click me
+        </Text>
+      </TouchableHighlight>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: buttonColor }]}
+        onPress={handleButtonPress}
+      >
+        <Text style={styles.buttonText}>Change color</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +50,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f9f5e3",
+  },
+  text: {
+    color: "#093A3E",
+    fontSize: 20,
+    margin: 20,
+  },
+  button: {
+    backgroundColor: "#62a87c",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#f9f5e3",
+    fontSize: 17,
   },
 });
